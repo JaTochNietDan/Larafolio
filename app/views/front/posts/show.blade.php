0dbe6b957515911642656c0dbae29552d62c9d41
JaTochNietDan <contact@jatochnietdan.com>
2014-01-05 22:36:17 +0000

    Added comments to posts

--- app/views/front/posts/show.blade.php
+++ app/views/front/posts/show.blade.php
@@ -14,7 +14,33 @@
             <p></p><p>{{ $post->content }}</p>
         </div>
     </div>
+    <div class="panel panel-default">				
+        <div class="panel-heading">
+            <span class="glyphicon glyphicon-comment"></span>
+            Comments
+        </div>
+    
+        <div class="panel-body">
+            <div id="disqus_thread"></div>
+            <script type="text/javascript">
+            var disqus_shortname = 'larafolio';
+            var disqus_identifier = 'post_{{ $post->id }}';
+
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+            <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+        </div>
+    </div>
 </div>
+@stop
+
+@section('title')
+    {{ $post->title }}
 @stop
 
 @section('breadcrumb')
