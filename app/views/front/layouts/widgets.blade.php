86e13802801e58deacfa3377a70e1bddd51126ff
JaTochNietDan <contact@jatochnietdan.com>
2014-01-15 15:40:41 +0000

    Removed useless div

--- app/views/front/layouts/widgets.blade.php
+++ app/views/front/layouts/widgets.blade.php
@@ -1,18 +1,16 @@
 @if(Cache::has('widgets'))
     @foreach(Cache::get('widgets') as $widget)
-    <div class="row-right">
-        <div class="panel panel-default">				
-            <div class="panel-heading">
-                {{ $widget['title'] }}
-            </div>
-        
-            <div class="panel-body">
-                @if($widget['type'] === 'custom')
-                    {{ $widget['content'] }}
-                @else
-                    @include('front.widgets.'.$widget['type'])
-                @endif
-            </div>
+    <div class="panel panel-default">				
+        <div class="panel-heading">
+            {{ $widget['title'] }}
+        </div>
+    
+        <div class="panel-body">
+            @if($widget['type'] === 'custom')
+                {{ $widget['content'] }}
+            @else
+                @include('front.widgets.'.$widget['type'])
+            @endif
         </div>
     </div>
     @endforeach
