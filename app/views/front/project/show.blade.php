9157760fa44716c825c13f432070691d1c416d9f
JaTochNietDan <contact@jatochnietdan.com>
2014-01-17 16:36:28 +0000

    Added comments for projects

--- app/views/front/project/show.blade.php
+++ app/views/front/project/show.blade.php
@@ -65,6 +65,30 @@
         @endif
         </div>
     </div>
+    @if(Cache::has('disqus'))
+    <div class="panel panel-default">				
+        <div class="panel-heading">
+            <span class="glyphicon glyphicon-comment"></span>
+            Comments
+        </div>
+    
+        <div class="panel-body">
+            <div id="disqus_thread"></div>
+            <script type="text/javascript">
+            var disqus_shortname = '{{ Cache::get('disqus') }}';
+            var disqus_identifier = 'post_{{ $project->id }}';
+
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+            <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+        </div>
+    </div>
+    @endif
 </div>
 @stop
 
